e0694fe1eeed9dee0f58e980f6ac381283423e65
racheliver <racheliver@gmail.com>
2020-11-18 11:33:37 +0200

    Add functionality to the button (add exercise to list) &  Delete from list

--- src/Components/ExerciseItem.js
+++ src/Components/ExerciseItem.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
-const ExerciseItem = () => {
+const ExerciseItem = ({text,todo,Exercises,setExercises}) => {
+const deleteHandler = () => {
+    setExercises(Exercises.filter((el) => el.id !== todo.id));        
+};
+const completeHandler = () => {
+    setExercises(Exercises.map(item => {
+        if(item.id === todo.id){
+            return{
+                ...item, completed: !item.completed
+            }
+        }
+        return item;
+    }));
+}
     return(
 
         <div className="Exercise">
-            <il className="Exercise-item">HEY YOU</il>
-            <button className="complete-btn" >
+            <il className="Exercise-item">{text}</il>
+            <button onClick={completeHandler} className="complete-btn" >
                 <i className="fas fa-check"></i>
             </button>
-            <button className="trash-btn">
+            <button onClick={deleteHandler} className="trash-btn">
                 <i className="fas fa-trash"></i>
             </button>
         </div>
--- src/Components/ExerciseList.js
+++ src/Components/ExerciseList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Exercise from "./ExerciseItem";
 
-const ExerciseList = () => {
+const ExerciseList = ( {Exercises,setExercises}) => {
     return(
         <div className="exercise-container">
             <ul className="exercise-list">
-                <Exercise />
+                {Exercises.map((todo) => (
+                    <Exercise  todo={todo} Exercises={Exercises} setExercises={setExercises} text={todo.text} key={todo.id} />
+                ))}
             </ul>
         </div>
     );
