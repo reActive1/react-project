aa445d782555b4884a938e3132697f74b30f7d02
keren <kerenomero@gmail.com>
2020-11-22 01:49:33 -0800

    list exercises and links to the exercises details

--- src/Common/Enums.js
+++ src/Common/Enums.js
@@ -1,27 +1,44 @@
 export const Exercises = {
-    StrongBack: "StrongBack",
-    PulseRows: "PulseRows",
-    ElbowToTheKnee: "ElbowToTheKnee",
-    StandingButterfly: "StandingButterfly",
-    Plank: "Plank",
-    JumpingJacks: "JumpingJacks",
-    Crunches: "Crunches",
-    Squat: "Squat",
-    SumoSquat: "SumoSquat",
-    SquatJump: "SquatJump",
-    SquatBackJump: "SquatBackJump",
-    RomanianDeadLift: "RomanianDeadLift",
-    Launge: "Launge",
-    LungeToKneeRise: "LungeToKneeRise",
-    LungeJumpToKneeRise: "LungeJumpToKneeRise",
-    SidePlank: "SidePlank",
-    BicycleCrunches: "BicycleCrunches",
-    PushUps: "PushUps",
-    AbdominalCrunch: "AbdominalCrunch",
-    WallSit: "WallSit",
-    TricepsDipOnChair: "TricepsDipOnChair",
-    HighKneesRunningInPlace: "HighKneesRunningInPlace",
-    PushUpAndRotation: "PushUpAndRotation",
-    ButtKicks: "ButtKicks"
+     Back: {
+           Supermans: "Supermans", // https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/9/supermans/
+           BirdDog: "Bird-dog",  //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/14/bird-dog/
+           CatCow: "Cat-Cow",   //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/15/cat-cow/
+           ChildsPose: "Childs Pose", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/227/childs-pose/
+           FrontPlank: "Front Plank" //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/32/front-plank/
+       },
+    Legs: {
+        ForwardLunge: "Forward Lunge", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/94/forward-lunge/
+        BodyweightSquat: "Bodyweight Squat",  //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/135/bodyweight-squat/
+        DownwardFacingDog: "Downward-facing Dog", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/18/downward-facing-dog/
+        ForwardLinearJumps: "Forward Linear Jumps", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/177/forward-linear-jumps/
+        MountainClimbers: "Mountain Climbers" //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/258/mountain-climbers/
+        },
+   Abs: {
+        Crunch: "Crunch", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/52/crunch/
+        DirtyDog: "Dirty Dog", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/109/dirty-dog/
+        GluteActivationLunges: "Glute Activation Lunges", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/96/glute-activation-lunges/
+        CobraExercise: "Cobra Exercise", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/16/cobra/
+        GluteBridgeExercise: "Glute Bridge Exercise", //Glute Bridge Exercise
+       },
+   Shoulders: {
+        BentKneePushUp: "Bent Knee Push-up", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/13/bent-knee-push-up/
+        ContralateralLimbRaises: "Contralateral Limb Raises", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/53/contralateral-limb-raises/
+        Inchworms: "Inchworms", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/254/inchworms/
+        ProneScapularShoulder: "Prone Scapular Shoulder",  //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/249/prone-scapular-shoulder-stabilization-series-i-y-t-w-o-formation/
+        PushUp: "Push-up" //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/41/push-up/
+        },
+   FullBody: {
+       BearCrawlExercise: "Bear Crawl Exercise", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/150/bear-crawl/
+       InvertedFlyers: "Inverted Flyers",//https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/144/inverted-flyers/
+       LateralCrawls : "Lateral Crawls", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/328/lateral-crawls/
+       SpiderWalks: "Spider Walks", // https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/247/spider-walks/
+       Warrior: "Warrior" //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/228/warrior-i/
+       },
+   Chest: {
+       CKCParascapularExercises: "CKC Parascapular Exercises", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/259/ckc-parascapular-exercises/
+       PlankUps: "Plank-ups", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/320/plank-ups/
+       PushUpWithSingleLegRaise: "Push-up with Single-leg Raise", //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/42/push-up-with-single-leg-raise/
+       StandingChestStretch: "Standing Chest Stretch", // https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/209/standing-chest-stretch/
+       SupineSpinalTwistWithRibGrab: "Supine Spinal Twist with Rib Grab and Progressions" //https://www.acefitness.org/education-and-resources/lifestyle/exercise-library/229/supine-spinal-twist-with-rib-grab-and-progressions/
+   }
 }
-
