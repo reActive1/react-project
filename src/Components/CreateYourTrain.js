ac2cf3341312e9124faf9a7cae06d21b446acc16
steinshi <steinshi@post.bgu.ac.il>
2020-11-22 14:18:32 +0200

    added clean component createYourTrain for team 3

---
+++ src/Components/CreateYourTrain.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class CreateYourTrain extends React.Component {
+    render(){
+        return <div className = "CreateYourTrain"><button>Start Train!</button></div>
+    }
+}
+
+export default CreateYourTrain;
