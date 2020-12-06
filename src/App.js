6b4b9ff809e45e530d7b3dc18a7ccd3f5d22da7d
yaelih <yael_h@yahoo.com>
2020-12-06 20:14:55 +0200

    (Shiran & Yael) Clean App.js routes code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import './App.css';
 import Sidebar from "./Components/Sidebar";
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from "./Components/Home";
-import Overview from "./Components/Overview";
-import CreateYourTrain from './Components/CreateYourTrain';
 import ChooseTotalTime from "./Components/ChooseTotalTime";
-import Timer from "./Components/Timer";
 import {SidebarData} from "./Components/SidebarData";
 import ChooseExrcise from "./ChooseExrcise";
+import Timer from "./Components/Timer";
 
 function App() {
-  // todo: use map for routes and also create routes for other components
   return (
     <Router> 
     <div className="App">
@@ -18,12 +15,9 @@ function App() {
       <Route path={"/"} exact component={Home}></Route>
       { SidebarData.map((item, index) => {
         return (
-          <Route key={index} path={item.link} exact component={item.component}> </Route>
+          <Route key={index} path={item.link} exact component={item.component}></Route>
         )
       })}
-      <Route path="/home" exact component={Home} /> 
-      <Route path="/overview" exact component={Overview} /> 
-      <Route path="/createYourTrain" exact component={CreateYourTrain} />
       <Route path="/ChooseTotalTime" component={ChooseTotalTime}/>
       <Route path="/ChooseExrcise"
                    render={(props) => (
