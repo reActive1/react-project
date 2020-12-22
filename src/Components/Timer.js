c357f99c457d98e7eb30bdbcd17b0f1c1be14285
kerenomero <kerenome19@gmail.com>
2020-12-22 16:00:03 +0200

    add timer connect to the exercise duration (on-going-need to fix dinamclly time)

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import "./CssComponents/Timer.css";
 import {FaPauseCircle, FaPlayCircle} from "react-icons/fa";
 import Countdown, {zeroPad} from "react-countdown";
-import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import StartExerciseList from "./StartExerciseList"
 import {useLocation} from 'react-router-dom'
 
@@ -12,7 +11,6 @@ function Timer(props) {
   let countdownApi = null;
   const [isPlaying, setIsPlaying] = useState(true);
   const [time, setTime] = useState(Date.now()+props.time); 
-  const [duration, setDuration] = useState(props.time/60/1000); //Should be updated with exercise time
 
   const setRef = countdown => {
     if (countdown) {
@@ -49,36 +47,10 @@ function Timer(props) {
       }
     }
 
-    //exercise time countdown settings
-    const renderTime = ({ remainingTime }) => {
-      if (remainingTime === 0) {
-        return <div className="timer">Great!</div>;
-      }
-      return (
-        <div className="timer"> 
-          <div className="text">Remaining</div>
-          <div className="value">{remainingTime}</div>
-          <div className="text">seconds</div>
-        </div>
-      );
-    };
 
     return (
       <div className="timers">
         <h1><Countdown date={time} renderer={renderer} ref={setRef} /></h1>
-        <div className="timer-wrapper">
-          <CountdownCircleTimer
-            isPlaying={isPlaying}
-            duration={duration}
-            colors={[
-              ['#004777', 0.33],
-              ['#F7B801', 0.33],
-              ['#A30000', 0.33],
-            ]}
-          >
-            {renderTime}
-          </CountdownCircleTimer>
-        </div>
         <div className="button-wrapper">
           {displayPauseOrResumeButton()}
         </div>
